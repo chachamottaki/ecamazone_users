0a27553a46bc63932fad26342a518d5ba926bb92
elandaloussi20 <anouar.elanda@gmail.com>
2023-11-07 23:38:49 +0100

    add dockerfiles db and api

--- app/db.js
+++ app/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 const sequelize = new Sequelize('user_microservice', 'root', 'root', {
-    host: 'localhost',
+    host: 'brave_tharp',
     dialect: 'mysql',
   });
 module.exports = sequelize
