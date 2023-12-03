317ae64a4f7d5653a4acd89d390ee8af90f187c5
elandaloussi20 <anouar.elanda@gmail.com>
2023-12-04 00:23:01 +0100

    update user model

--- app/models/userModel.js
+++ app/models/userModel.js
@@ -10,7 +10,7 @@ const User = db.define('User', {
     fullName: {
       type: Sequelize.STRING,
       allowNull: false,
-  },
+    },
     email: {
       type: Sequelize.STRING,
       allowNull: false,
@@ -25,32 +25,11 @@ const User = db.define('User', {
     shippingAddress: {
       type: Sequelize.STRING,
       // Vous pouvez diviser en plusieurs champs (rue, ville, code postal, pays) si nécessaire
-  },
+    },
   isAdmin: {
     type: Sequelize.BOOLEAN,
     defaultValue: false, // Définit la valeur par défaut à 'false'
-},
-//   billingAddress: {
-//       type: Sequelize.STRING,
-//       // Vous pouvez diviser en plusieurs champs si nécessaire
-//   },
-//   cardHolderName: {
-//     type: Sequelize.STRING,
-// },
-// cardLastFourDigits: {
-//     type: Sequelize.STRING,
-//     validate: {
-//         len: [4, 4],
-//     },
-// },
-// cardExpirationDate: {
-//     type: Sequelize.STRING,
-//     // Valider le format de la date si nécessaire
-// },
-// cardType: {
-//     type: Sequelize.STRING,
-//     // Visa, MasterCard, etc.
-// },
+    },
     password: {
       type: Sequelize.STRING,
     },
