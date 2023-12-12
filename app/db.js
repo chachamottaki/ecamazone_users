02ddd3fe920e577cc22c5528813840654538b390
elandaloussi20 <anouar.elanda@gmail.com>
2023-12-12 14:50:18 +0100

    update kubernetes files

--- app/db.js
+++ app/db.js
@@ -4,8 +4,13 @@ const sequelize = new Sequelize(
   process.env.TEST_DB_NAME || 'user_microservice',
   process.env.TEST_DB_USER || 'root',
   process.env.TEST_DB_PASSWORD || 'secret',
+  // process.env.TEST_DB_PASSWORD || 'root',
+
+  
+
   {
     host: process.env.TEST_DB_HOST || 'mysql-service',
+    // host: process.env.TEST_DB_HOST || 'localhost',
     dialect: 'mysql',
     port: process.env.TEST_DB_PORT || 3306
   }
